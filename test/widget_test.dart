55e292f6005696fefcd801a15d4c725757f8287a
jay_wu <jay_wu>
2021-05-09 19:00:40 +0800

    加入基本程式碼與套件 （starter）

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,7 +5,7 @@ import 'package:flutter_test_practice_1/app/app.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(TestingApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
