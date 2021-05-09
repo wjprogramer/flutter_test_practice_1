92dbdf73fb3a436966f1b145ef728d01626394ff
jay_wu <jay_wu>
2021-05-09 19:36:51 +0800

    完成後的程式碼

---
+++ test/models/favourites_test.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_test_practice_1/models/favorites.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Testing App Provider Tests', () {
+    var favorites = Favorites();
+
+    test('A new item should be added', () {
+      var number = 35;
+      favorites.add(number);
+      expect(favorites.items.contains(number), true);
+    });
+
+    test('An item should be removed', () {
+      var number = 45;
+      favorites.add(number);
+      expect(favorites.items.contains(number), true);
+      favorites.remove(number);
+      expect(favorites.items.contains(number), false);
+    });
+  });
+}
